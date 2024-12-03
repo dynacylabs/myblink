cbd6c3ce8bf7efe2ae7c43e3376a2b1a1e370e42
Austin Conn <austinc3030@gmail.com>
2024-12-03 12:29:46 -0500

    Tweaks

--- startup.sh
+++ startup.sh
@@ -4,3 +4,8 @@ python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+python myblink.py
+
+# Keep container running if app stops so we can debug
+tail -f /dev/null
